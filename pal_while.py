12c898e942f6a72859b4a981838c4a5abd682bd7
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-22 10:34:43 -0400

    modified code to not hit an error. A bug still exists.

--- pal_while.py
+++ pal_while.py
@@ -1,20 +1,35 @@
-pal_list = ["Anna", "Racecar", "control", "Kayak"]
+pal_list = ["anna", "racecar", "control", "kayak"]
 palindrome = True
 
-def is_palindrome(pal_list, palindrome):
-    while True:
+def is_palindrome(pal_list,palindrome):
+    while palindrome:
         in_word = str(input("Enter a word: "))
         pal_list.append(in_word.lower())
         print(pal_list)
-        for pal in pal_list:
+        ask_word = input("Would you like to enter another word: 'yes' or 'no'")
+        if ask_word == 'no':
+            break
+    return pal_list
+
+def main(palindrome):    
+    for pal in is_palindrome(pal_list,palindrome):
+        for p in range(len(pal)):
+            if pal[p] != pal[len(pal)-p-1]:
+                palindrome = False
+        if palindrome == True:
+            print(f'{pal} is a palindrome.')
+        else:
+            print(f'{pal} is not a palidrome.')
+
+if __name__ == "__main__":
+    main(palindrome)
+
+'''
+for pal in pal_list:
             for p in range(len(pal)//2):
                 if pal[p] != pal[len(pal) - p -1]:
                     palindrome = False
                     print(f"{pal} is not a palidrome.")
                 else:
                     print(f"{pal} is a palindrome.")
-        ask_word = input("Would you like to enter another word: 'yes' or 'no'")
-        if ask_word == 'no':
-            break
-        
-is_palindrome(pal_list, palindrome)
+'''
